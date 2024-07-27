723bd3a6eead029a7d6a8d06dd15a497dd97a0fb
msinggiadityaramadhan <msinggiadityaramadhan@gmail.com>
2024-07-27 08:28:12 +0000

    "fix: null safety in fullscreen image"

--- lib/src/config/themes/app_textstyles.dart
+++ lib/src/config/themes/app_textstyles.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 
-TextStyle titleSegoeUITextStyle = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 16);
-TextStyle subtitleSegoeUITextStyle = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 14);
-TextStyle regularSegoeUITextStyle = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w400, fontSize: 12);
+TextStyle titleSegoeUITextStyle = const TextStyle(
+    fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 16);
+TextStyle subtitleSegoeUITextStyle = const TextStyle(
+    fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 14);
+TextStyle regularSegoeUITextStyle = const TextStyle(
+    fontFamily: "Segoe UI", fontWeight: FontWeight.w400, fontSize: 12);
 
-TextStyle titleProximaNovaTextStyle = const TextStyle(fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 16);
-TextStyle subtitleProximaNovaTextStyle = const TextStyle(fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 14);
-TextStyle regularProximaNovaTextStyle = const TextStyle(fontFamily: "Proxima Nova", fontWeight: FontWeight.w400, fontSize: 12);
+TextStyle titleProximaNovaTextStyle = const TextStyle(
+    fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 16);
+TextStyle subtitleProximaNovaTextStyle = const TextStyle(
+    fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 14);
+TextStyle regularProximaNovaTextStyle = const TextStyle(
+    fontFamily: "Proxima Nova", fontWeight: FontWeight.w400, fontSize: 12);
+
+// TextStyle h2_1 = const TextStyle(
+//     fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 15);
+// TextStyle h2_3 = const TextStyle(
+//     fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 16);
+// TextStyle h2_4 = const TextStyle(
+//     fontFamily: "Proxima Nova", fontWeight: FontWeight.w700, fontSize: 20);
+
+// TextStyle h3_1 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 16);
+// TextStyle h3_2 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 14);
+// TextStyle h3_3 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 17);
+// TextStyle h3_6 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 15);
+// TextStyle h3_7 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 30);
+// TextStyle h3_8 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 20);
+// TextStyle h3_8 = const TextStyle(fontFamily: "Segoe UI", fontWeight: FontWeight.w700, fontSize: 23);
+
