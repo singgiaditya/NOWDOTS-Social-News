aadb8cc56dbb42dfe52e34508f0a5abd000c38fd
msinggiadityaramadhan <msinggiadityaramadhan@gmail.com>
2024-08-04 08:59:47 +0000

    fix: regex validation username

--- lib/src/core/utils/regex_patterns.dart
+++ lib/src/core/utils/regex_patterns.dart
@@ -1,8 +1,9 @@
 class RegexPatterns {
   static final RegExp emailOrUsernameRegex = RegExp(
-      r'^(?:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|@[a-zA-Z0-9_]+)$');
+      r'^(?:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}|[a-zA-Z0-9_.\-]{1,})$');
   static final RegExp emailRegex =
       RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
-  static final RegExp usernameRegex = RegExp(r'^@[a-zA-Z0-9_]+$');
+  static final RegExp usernameRegex = RegExp(r'^[a-zA-Z0-9_.\-]{1,}$');
+
   static final RegExp hashtagAndUsernameRegex = RegExp(r'[#@]\w+');
 }
